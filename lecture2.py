bdc3cf71597d4b4e2ce93584b762bb3efdd8129e
nikolaevroma <72045521+nikolaevroma@users.noreply.github.com>
2020-10-05 23:59:27 +0600

    Add files via upload

---
+++ lecture2.py
@@ -0,0 +1,233 @@
+number=int(input("Введи число(0-99): "))
+q="один"
+w="два"
+e="три"
+r="четыре"
+t="пять"
+y="шесть"
+u="семь"
+i="восемь"
+o="девять"
+p="десять"
+a="одиннадцать"
+s="двенадцать"
+d="тринадцать"
+f="четырнадцать"
+g="пятнадцать"
+h="шестнадцать"
+j="семнадцать"
+k="восемнадцать"
+l="девятнадцать"
+W="двадцать"
+E="тридцать"
+R="сорок"
+T="пятьдесят"
+Y="шесьтьдесят"
+U="семьдесят"
+I="восемьдесят"
+O="девяносто"
+if 0<=number<=99:
+    if number==0:
+        print("ноль")
+    if number==1:
+        print(q)
+    if number==2:
+        print(w)
+    if number==3:
+        print(e)
+    if number==4:
+        print(r)
+    if number==5:
+        print(t)
+    if number==6:
+        print(y)
+    if number==7:
+        print(u)
+    if number==8:
+        print(i)
+    if number==9:
+        print(o)
+    if number==10:
+        print(p)
+    if number==11:
+        print(a)
+    if number==12:
+        print(s)
+    if number==13:
+        print(d)
+    if number==14:
+        print(f)
+    if number==15:
+        print(g)
+    if number==16:
+        print(h)
+    if number==17:
+        print(j)
+    if number==18:
+        print(k)
+    if number==19:
+        print(l)
+    if number==20:
+        print(W)
+    if number==21:
+        print(W, q)
+    if number==22:
+        print(W, w)
+    if number==23:
+        print(W, e)
+    if number==24:
+        print(W, r)
+    if number==25:
+        print(W, t)
+    if number==26:
+        print(W, y)
+    if number==27:
+        print(W, u)
+    if number==28:
+        print(W, i)
+    if number==29:
+        print(W, o)
+    if number==30:
+        print(E)
+    if number==31:
+        print(E, q)
+    if number==32:
+        print(E, w)
+    if number==33:
+        print(E, e)
+    if number==34:
+        print(E, r)
+    if number==35:
+        print(E, t)
+    if number==36:
+        print(E, y)
+    if number==37:
+        print(E, u)
+    if number==38:
+        print(E, i)
+    if number==39:
+        print(E, o)
+    if number==40:
+        print(R)
+    if number==41:
+        print(R, q)
+    if number==42:
+        print(R, w)
+    if number==43:
+        print(R, e)
+    if number==44:
+        print(R, r)
+    if number==45:
+        print(R, t)
+    if number==46:
+        print(R, y)
+    if number==47:
+        print(R, u)
+    if number==48:
+        print(R, i)
+    if number==49:
+        print(R, o)
+    if number==50:
+        print(T)
+    if number==51:
+        print(T, q)
+    if number==52:
+        print(T, w)
+    if number==53:
+        print(T, e)
+    if number==54:
+        print(T, r)
+    if number==55:
+        print(T, t)
+    if number==56:
+        print(T, y)
+    if number==57:
+        print(T, u)
+    if number==58:
+        print(T, i)
+    if number==59:
+        print(T, o)
+    if number==60:
+        print(Y)
+    if number==61:
+        print(Y, q)
+    if number==62:
+        print(Y, w)
+    if number==63:
+        print(Y, e)
+    if number==64:
+        print(Y, r)
+    if number==65:
+        print(Y, t)
+    if number==66:
+        print(Y, y)
+    if number==67:
+        print(Y, u)
+    if number==68:
+        print(Y, i)
+    if number==69:
+        print(Y, o)
+    if number==70:
+        print(U)
+    if number==71:
+        print(U, q)
+    if number==72:
+        print(U, w)
+    if number==73:
+        print(U, e)
+    if number==74:
+        print(U, r)
+    if number==75:
+        print(U, t)
+    if number==76:
+        print(U, y)
+    if number==77:
+        print(U, u)
+    if number==78:
+        print(U, i)
+    if number==79:
+        print(U, o)
+    if number==80:
+        print(I)
+    if number==81:
+        print(I, q)
+    if number==82:
+        print(I, w)
+    if number==83:
+        print(I, e)
+    if number==84:
+        print(I, r)
+    if number==85:
+        print(I, t)
+    if number==86:
+        print(I, y)
+    if number==87:
+        print(I, u)
+    if number==88:
+        print(I, i)
+    if number==89:
+        print(I, o)
+    if number==90:
+        print(O)
+    if number==91:
+        print(O, q)
+    if number==92:
+        print(O, w)
+    if number==93:
+        print(O, e)
+    if number==94:
+        print(O, r)
+    if number==95:
+        print(O, t)
+    if number==96:
+        print(O, y)
+    if number==97:
+        print(O, u)
+    if number==98:
+        print(O, i)
+    if number==99:
+        print(O, o)
+elif number<0:
+    print("Введите положительное число!")
+else:
+    print("Попробуйте ввести число от 0 до 99 ещё раз!")
